45f5dd65c0b29fd41978ca6a05473bbe127fc38f
TRIGONtechh <167967248+TRIGONtechh@users.noreply.github.com>
2024-04-26 12:35:28 +0530

    Add files via upload

---
+++ 2 variable.py
@@ -0,0 +1,20 @@
+a = 1234567890
+print(a,type(a))
+b = "yashash"
+print(b,type(b))
+c = True
+print(c,type(c))
+# here type shows is it a int,string or float etc
+
+
+
+print(str(a) + b)   
+
+# to use list 
+list1= [0,3,4,"yashash","raichur",[1,4,6,],["applle","banana"]]
+print(list1)
+
+# dictioary used to store data in key value pair
+dict1={"name":"yashash","age":18,"class":"1st year","canvote":True}
+print(dict1)
+#  'name':'yashash','age':18
