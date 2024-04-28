51bd7b1105656530084ab41acdf198375dde69b0
TRIGONtechh <167967248+TRIGONtechh@users.noreply.github.com>
2024-04-28 21:46:56 +0530

    Delete 5 user input.py

--- 5 user input.py
+++
@@ -1,8 +0,0 @@
-a = input("Enter your name ")
-print("Hello ",a," how r u")
-
-x = input("Class name ")
-y = input("Roll no ")
-print(a,int(x),y)
-
-# bur we cant convernt name/string/albaet to num but num to string
